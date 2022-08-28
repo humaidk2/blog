1d2d9b4f6984702a0e418ed096b9ad39d0608f24
Humaid Arif Khan <humaid.k@tlt.local>
2022-08-28 01:11:14 +0100

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,34 @@
-import '../styles/index.css'
+import Head from "next/head";
+import React from "react";
+import "../styles/index.css";
+
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
+
+      <Head>
+        <title>Welcome!</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
